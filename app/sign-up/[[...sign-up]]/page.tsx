32e456daad3de938089f64683ca245243260cb6e
Gary Birrell <garybirrell@Garys-MacBook-Pro.local>
2026-06-03 16:08:59 +0100

    Add native sign-in pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="flex justify-center items-center min-h-[70vh] bg-neutral-950 py-16">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
     </div>
   );
 }

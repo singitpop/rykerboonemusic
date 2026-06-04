5d1034fa08a66a179c1fe4e929364f30a4da1354
Gary Birrell <garybirrell@Garys-MacBook-Pro.local>
2026-06-04 09:09:58 +0100

    Fix 500 error by forcing dynamic rendering on radio page

--- app/club/radio/page.tsx
+++ app/club/radio/page.tsx
@@ -4,6 +4,8 @@ import { getRykerSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function RadioPage() {
   const session = await getRykerSession();
 
